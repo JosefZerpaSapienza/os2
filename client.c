b0e72d70e590c40f876fafe283a43f475020f13a
Josef Zerpa <zerparuiz.1837394@studenti.uniroma1.it>
2021-06-17 16:15:21 +0200

    Fix pipe checking and signal catching. Add write to log file in both server and client. Add mutex for accessing shared data in server.c

--- client.c
+++ client.c
@@ -13,27 +13,32 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
 
 #define BUFF_SIZE 256
 #define TIMEOUT 1
+#define GLOBAL_LOG_FILENAME "client_global.log"
 #define handle_err(msg) \
   do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-int QUIT;
+int quit = 0;
 
-void quit (int signo) {
-  if (QUIT) { return; } // quit() already run
+static void *receiver (void *);
 
-  QUIT = 1;
+void clean (int signo) {
+  if (quit) { return; } // clean() already run
+
+  quit = 1;
 }
 
 int main (int argc, char **argv) {
   int connfd, portno, n;
   struct sockaddr_in serv_addr;
   char buff[BUFF_SIZE];
-  struct sigaction action;
-  struct timeval timeout;
-  fd_set writefds;
+  struct sigaction action, ign_action;
+  mode_t mode;
+  int global_log_fd;
+  pthread_t thread_id;
 
   // Check options.
   if (argc != 3) {
@@ -58,35 +63,28 @@ int main (int argc, char **argv) {
     handle_err("Error trying to connect.");
   }
 
-  
-  // Spawn writer thread.
-  // TODO: writer thread.
+  // Spawn receiver thread.
+  if (pthread_create(&thread_id, NULL, &receiver, &connfd) != 0) {
+    handle_err("Error on thread creation.");
+  }
   
   // Sender thread.
   // Exit when SIGINT is caught.
-  action.sa_handler = &quit;
+  action.sa_handler = &clean;
   action.sa_flags = SA_RESETHAND; // reset to default after use
   sigaction(SIGINT, &action, NULL);
-  sigaction(SIGPIPE, &action, NULL);
+  ign_action.sa_handler = SIG_IGN;
+  sigaction(SIGPIPE, &ign_action, NULL);
 
   printf("Connected to %s.\n", argv[1]);
   printf("\nPress CTRL-C to exit.\n");
   printf("Type messages and click enter to send.\n");
   printf("-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --\n\n");
   
-  // Set pipe checker
-  timeout.tv_sec = TIMEOUT;
-  timeout.tv_usec = 0;
-  FD_ZERO(&writefds);
-  FD_SET(connfd, &writefds);
-  QUIT = 0;
-  while(!QUIT) {
-    // Read input and send messages
-    // TODO:
+  while(!quit) {
+    // Read input and send messages. Also check pipe.
     fgets(buff, BUFF_SIZE, stdin);
-    //Check if pipe was closed.
-    if ( (select(connfd + 1, NULL, &writefds, NULL, &timeout)) <= 0 ) { quit(0); }
-    write(connfd, buff, strlen(buff));
+    if ( (write(connfd, buff, strlen(buff))) == -1 && errno == EPIPE) { break; }
   }
 
   printf("\n-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --\n\n");
@@ -95,3 +93,33 @@ int main (int argc, char **argv) {
 
   return 0;
 }
+
+
+// Receiver/Writer thread.
+// Receive messages and write them to disk.
+static void *receiver (void *arg) {
+  char buff[BUFF_SIZE];
+  int connfd = *((int *) arg);
+  int n;
+
+  // Open global log file.
+  mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+  int global_log_fd = open(GLOBAL_LOG_FILENAME, O_RDWR | O_CREAT | O_APPEND, mode);  
+
+  while(1) {
+    n = read(connfd, buff, BUFF_SIZE - 1);
+    if (!n) { break; } // Check if pipe was closed!
+
+    buff[n] = '\0';
+    // Print on screen.
+    printf("%s", buff);
+
+    // Write on disk.
+    if ( (write(global_log_fd, buff, n)) <= 0) {
+      handle_err("Error writing on global log.");
+    }
+  }
+
+  close(global_log_fd);
+}
+
--- server.c
+++ server.c
@@ -15,22 +15,41 @@
 #include <pthread.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <fcntl.h>
 #include "structures.h"
 
-#define BUFF_SIZE 256
+#define BUFF_SIZE 128 // TODO: Check what happens with a bigger input
 #define TIMEOUT 120
+#define MSG_STACK_SIZE 10
+#define GLOBAL_LOG_FILENAME "server_global.log"
+
 #define handle_err(msg) \
   do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
 // Clean up before exiting.
-void quit (int signo);
+void clean (int);
 // Receiver thread.
-static void *receiver (void *arg);
+static void *receiver (void *);
+// Writer thread.
+static void *writer (void *);
 
 // Switch that keeps the server listening for new connections.
-static int QUIT = 0;
+static int quit = 0;
 // List of active connections.
 static struct ConnList *connections;
+// Shared stack of messages.
+static char msg_stack[MSG_STACK_SIZE][BUFF_SIZE];
+
+pthread_mutex_t stack_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t connections_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t new_msg_cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t stack_cond = PTHREAD_COND_INITIALIZER;
+static int stack_availability = MSG_STACK_SIZE;
+static int stack_index = 0;
+static int new_msg = 0;
+static int write_index = 0;
+static int read_index = 0;
+
 
 // Chatroom server. 
 // Listen for connections and manage thread creation
@@ -43,7 +62,9 @@ int main (int argc, char **argv) {
   int client_addr_size;
   pthread_t thread_id;
   struct Node *connection;
-  struct sigaction action, old_action;
+  struct sigaction action, old_action, ign_action;
+  mode_t mode;
+  int global_log_fd;
 
   // Check options
   switch (argc) 
@@ -89,32 +110,37 @@ int main (int argc, char **argv) {
   connections = (struct ConnList *) malloc(sizeof(struct ConnList));
   connections->head = connections->tail = NULL;
   
+
   // Set up Writer thread.
-  // TODO: writer thread.
+  if (pthread_create(&thread_id, NULL, &writer, &global_log_fd) != 0) {
+    handle_err("Error on thread creation.");
+  }
 
   // Clean when SIGINT is caught.
-  action.sa_handler = &quit;
+  action.sa_handler = &clean;
   action.sa_flags = SA_RESETHAND; //reset to default after use
   sigaction(SIGINT, &action, NULL);
+  // Ignore SIGPIPE.
+  ign_action.sa_handler = SIG_IGN;
+  sigaction(SIGPIPE, &ign_action, NULL);
   
   // Listen for connections.
   printf("\nPress CTRL-C to exit.\n");
   printf("Listening for incoming connections.\n");
   printf("-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --\n\n");
-  QUIT = 0;
-  while(!QUIT) {
+  quit = 0;
+  while(!quit) {
     client_addr_size = sizeof(struct sockaddr_in);
     connfd = accept(listenfd, (struct sockaddr*) &client_addr, &client_addr_size);
     if (connfd == -1) {
-      quit(0); // SIGINT was received
+      clean(0); // SIGINT was received
       if (errno != EINTR) { // or other error
         handle_err("Error on accepting connection."); 
       }
-    } else {
-      // Connection received.
+    } else { // Connection received.
       // Create and append connection to connection list.
       if (! (connection = createConn(connfd, NULL, NULL))) {
-        quit(0);
+        clean(0);
 	handle_err("Error creating new connection node!"); 
       }
       appendConn(connection, connections);
@@ -123,30 +149,35 @@ int main (int argc, char **argv) {
       printf("%s connected.\n", client_ip);
       // Create thread and pass connection.
       if (pthread_create(&thread_id, NULL, &receiver, connection) != 0) {
-        quit(0);
-        handle_err("Error on creating thread.");
+        clean(0);
+        handle_err("Error on thread creation.");
       }
     }
   }
   
+  close(global_log_fd);
   printf("\nDone.\n");
 
 
   return 0;
 }
 
-// Change QUIT value, to quit the main running loop.
-void quit(int signo) {
+// Change QUIT value, to quit the main running loop,
+// and clean up dynamically allocated memory.
+void clean(int signo) {
   struct Node *node, *temp;
 
-  if (QUIT) { return; } // quit() already run
+  if (quit) { return; } // clean() already run
   
-  QUIT = 1;
+  quit = 1;
 
   printf("\n-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --\n\n");
   // Close open connections and 
   // free dynamically allocated memory.
   printf("Closing connections:\n");
+  if ( (pthread_mutex_lock(&connections_mutex)) != 0) { // Mutex read/write
+    handle_err("Error locking mutex.");
+  }
   node = connections->head;
   while(node) {
     printf("Close %d.\n", node->value);
@@ -158,42 +189,112 @@ void quit(int signo) {
     free(temp);
   }
   free(connections);
+  if ( (pthread_mutex_unlock(&connections_mutex)) != 0) {
+    handle_err("Error unlocking mutex.");
+  }
 }
 
 // Receiver thread.
-// Handles the connection with a client and
-// receives incoming messages.
+// Handles incoming messages from a client.
 static void *receiver(void *arg) {
   struct Node *connection = (struct Node *) arg;
   int connfd = connection->value;
-  int retval, n;
+  int n, welcome;
 
   char buff[BUFF_SIZE];
   const char welcome_msg[] = "Welcome in the chatroom. You are fd #%d.\n";
-  struct timeval timeout;
-  fd_set readfds;
   
   // Send welcome message.
   sprintf(buff, welcome_msg, connfd);
-  write(connfd, buff, strlen(buff));
+  n = write(connfd, buff, strlen(buff));
+  welcome = (n == -1 && errno == EPIPE) ? 0 : 1; // welcome = if welcome msg was received
 
-  // Check if pipe was closed!
-  timeout.tv_sec = TIMEOUT;
-  timeout.tv_usec = 0;
-  FD_ZERO(&readfds);
-  FD_SET(connfd, &readfds);
-  while(select(connfd + 1, &readfds, NULL, NULL, &timeout) > 0) {
-    // TODO: Read message and forward to writer thread.
+  while(welcome && !quit) {
     n = read(connfd, buff, BUFF_SIZE - 1);
-    if (n <= 0) { break; }
-    buff[n - 1] = '\0';
-    puts(buff);
+    if (!n) { break; } // Check if pipe was closed!
+
+    if (pthread_mutex_lock(&stack_mutex) != 0) { // Mutex write
+      handle_err("Error locking message stack mutex.\n");
+    }
+    while(!stack_availability) { // loop for signals interrupting the wait
+      pthread_cond_wait(&stack_cond, &stack_mutex);
+    }
+    snprintf(msg_stack[write_index++], n, buff); // Write in shared stack.
+    if (write_index == MSG_STACK_SIZE) { write_index = 0; }// Reset to use stack round-robin
+    new_msg++;
+    stack_availability--;
+    pthread_cond_signal(&new_msg_cond);
+    if ( (pthread_mutex_unlock(&stack_mutex)) != 0) {  
+      handle_err("Error unlocking message stack mutex.");
+    }
   }
 
   // When client disconnects remove fd from list
   // and return.
   printf("Connection #%d interrupted.\n", connfd);
+  if ( (pthread_mutex_lock(&connections_mutex)) != 0) { // Mutex write
+    handle_err("Error locking mutex.");
+  }
   removeConn(connection, connections);
+  if ( (pthread_mutex_unlock(&connections_mutex)) != 0) {
+    handle_err("Error unlocking mutex.");
+  }
 }
 
+// Writer thread.
+// Writes to disk and send to every client the received messages.
+static void *writer (void *arg) {
+  char buff[BUFF_SIZE];
+  int len;
+  struct Node *node;
+
+  // Open global log file.
+  mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+  int global_log_fd = open(GLOBAL_LOG_FILENAME, O_RDWR | O_CREAT | O_APPEND, mode);
+
+  while(1) {
+    if (pthread_mutex_lock(&stack_mutex) != 0) { // Mutex read
+      handle_err("Error locking message stack mutex.\n");
+    }
+    while(!new_msg) { // loop for signals interrupting the wait
+      pthread_cond_wait(&new_msg_cond, &stack_mutex);
+    }
+    // Read one message from the shared stack.
+    snprintf(buff, BUFF_SIZE, msg_stack[read_index++]);
+    if (read_index == MSG_STACK_SIZE) { read_index = 0; }// Reset to use stack round-robin
+    new_msg--;
+    stack_availability++;
+    pthread_cond_signal(&stack_cond);
+    if ( (pthread_mutex_unlock(&stack_mutex)) != 0) {
+      handle_err("Error unlocking message stack mutex.");
+    }
+
+    // Format message. TODO: Add timestamp.
+    len = strlen(buff);
+    buff[len] = '\n';
+    buff[len + 1] = '\0';
+    len++;
+
+    // Send message to every client.
+    if ( (pthread_mutex_lock(&connections_mutex)) != 0) { // Mutex read/write
+      handle_err("Error locking connections mutex.");
+    }
+    for(node = connections->head; node; node = node->next) {
+      // Send and check if pipe is broken
+      if( (write(node->value, buff, len)) == -1 && errno == EPIPE) {
+        removeConn(node, connections);
+      }	
+    }
+    if ( (pthread_mutex_unlock(&connections_mutex)) != 0) {
+      handle_err("Error unlocking connections mutex.");
+    }
+
+    // Write on disk.
+    if ( (write(global_log_fd, buff, len)) <= 0) {
+      handle_err("Error writing on global log.");
+    }
+  }
+
+  close(global_log_fd);
+}
 
